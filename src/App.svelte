7a8774dde5b1f3ec85f97d07ff78b7f1041189dd
Jessica F Martinez <jessicafndez@gmail.com>
2022-01-17 21:19:29 +0100

    first of all

---
+++ src/App.svelte
@@ -0,0 +1,33 @@
+<script>
+	// styles
+	import 'app.css';
+
+	// components
+	import Navbar from './components/Navbar.svelte';
+    import Main from './components/Main.svelte';
+</script>
+
+<main>
+	<Navbar />
+	<Main />
+</main>
+
+<style lang="postcss" global>
+	@tailwind base;
+	@tailwind components;
+	@tailwind utilities;
+
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
